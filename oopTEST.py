332f117d36576cc674e5bdcddfb14bb8331e2ef7
Dauren Tazhimuratov <tazhimuratovdauren@gmail.com>
2024-10-14 12:43:08 +0500

    first name

--- oopTEST.py
+++ oopTEST.py
@@ -1,1 +1,16 @@
-ff = 'ff'
+# 4-нұсқа  
+class Fruit:
+    def __init__(self, name, taste):
+        self.__name = name 
+        self.__taste = taste
+    def get_name(self):
+        return self.__name
+    def set_name(self, name):
+        self.__name = name
+    def get_taste(self):
+        return self.__name
+    def set_taste(self, taste):
+        self.__taste = taste
+
+F = Fruit("Tomato", 'Red') 
+print(F.get_name)
