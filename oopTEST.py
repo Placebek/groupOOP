f0fa5772c205154c2ed38c05a4516b3559e58924
Dauren Tazhimuratov <tazhimuratovdauren@gmail.com>
2024-10-14 13:33:54 +0500

    first commit

--- oopTEST.py
+++ oopTEST.py
@@ -1,16 +1,21 @@
-# 4-нұсқа  
+# 4 - нұсқа 
 class Fruit:
     def __init__(self, name, taste):
         self.__name = name 
         self.__taste = taste
+        
     def get_name(self):
         return self.__name
+    
     def set_name(self, name):
         self.__name = name
+        
     def get_taste(self):
-        return self.__name
+        return self.__taste  
+    
     def set_taste(self, taste):
         self.__taste = taste
 
-F = Fruit("Tomato", 'Red') 
-print(F.get_name)
+F = Fruit("Apple", 'Sweet') 
+
+print(F.get_name())  
