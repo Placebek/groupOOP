84238df602143aa140c15334ae527d2179da1114
Dani09934 <153008710+Dani09934@users.noreply.github.com>
2024-10-17 18:46:11 +0500

    Update oopTEST.py

--- oopTEST.py
+++ oopTEST.py
@@ -1,8 +1,8 @@
-# 4 - нұсқа 
+# 4 - нұсқа
 class Fruit:
     def __init__(self, name, taste):
-        self.__name = name 
-        self.__taste = taste
+        self.__name = name  
+        self.__taste = taste  
         
     def get_name(self):
         return self.__name
@@ -11,11 +11,49 @@ class Fruit:
         self.__name = name
         
     def get_taste(self):
-        return self.__taste  
+        return self.__taste
     
     def set_taste(self, taste):
         self.__taste = taste
+    
+    def describe(self):
+        return "Это настоящий фрукт"
 
-F = Fruit("Apple", 'Sweet') 
+class Apple(Fruit):
+    def __init__(self):
+        super().__init__("Apple", "Crunchy")
+    
+    def crunch(self):
+        return "Яблоко начинает хрустеть!"
+    
+    def describe(self):
+        return "Это яблоко"
 
-print(F.get_name())  
+class Orange(Fruit):
+    def __init__(self):
+        super().__init__("Orange", "Citrusy")
+    
+    def peel(self):
+        return "Апельсин очищается от кожуры"
+    
+    def describe(self):
+        return "Это апельсин"
+
+class Banana(Fruit):
+    def __init__(self):
+        super().__init__("Banana", "Soft")
+    
+    def peel(self):
+        return "Банан очищается от кожуры"
+    
+    def describe(self):
+        return "Это банан"
+
+fruits = [Apple(), Orange(), Banana()]
+
+for fruit in fruits:
+    print(fruit.describe())
+    if isinstance(fruit, Apple):
+        print(fruit.crunch())
+    if isinstance(fruit, Orange) or isinstance(fruit, Banana):
+        print(fruit.peel())
