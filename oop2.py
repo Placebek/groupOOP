6f8466ea2706701eac95fe2264248d32fbef7bae
usnv999 <baglan.usenoff@gmail.com>
2024-11-11 11:32:37 +0500

    1 Нұсқа

---
+++ oop2.py
@@ -0,0 +1,18 @@
+class Library:
+    def _init_(self,books,collection):
+        self.books = books
+        self.collection = collection
+
+    def count(self,sum,res):
+        get.add(self);
+        sum == 0
+        res = sum + 1
+    
+
+class Book:
+    def _init_(self,count):
+        self.count = count
+
+    def _add_books_(self):
+        sum == 1
+        res = sum - 1
