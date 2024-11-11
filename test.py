6c89d2d73632e782fae2efb4f7b8cf4f97f22304
G0nner <asknurkeldi@gmail.com>
2024-11-11 11:23:00 +0500

    11.11.24 5 nuska

--- test.py
+++ test.py
@@ -1,59 +1,58 @@
+
 class Appliance:
-	def __init__(self,brand, power):
-		self.__brand = brand
-		self.__power = power
+    
+    def __init__(self,brand, power):
+        self.__brand = brand
+        self.__power = power
 	
-	def get_brand(self):
-		return self.__brand
-	def get_power(self):
-		return self.__power
-	def set_brand(self, brand):
-		self.__brand = brand
-	def set_power(self, power):
-		self.__power = power
+    def get_brand(self):
+        return self.__brand
+        
+    def get_power(self):
+        return self.__power
+	
+    def set_brand(self, brand):
+        self.__brand = brand
+    def set_power(self, power):
+        self.__power = power
 
 class WashingMachine(Appliance):
-	def __init__(self,brand, power):
-		self.__brand = brand
-		self.__power = power
-	def wash(self):
-		print("Стиральная машина стирает")
-	def operate(self):
-		print("BRRRR")
+	    
+    def wash(self):
+        print("Styralnya mashina styraet")
+    def operate(self):
+        print("BRRRR")
 	
 class Refrigerator(Appliance):
-	def __init__(self,brand, power):
-		self.__brand = brand
-		self.__power = power
-	def cool(self):
-		print("Holodilnik ohlozhdaet")
-	#@overriding
-	def operate(self):
-		print("Bzzzz")
+	
+    def cool(self):
+        print("Holodilnik ohlozhdaet")
+	
+    #@overriding
+    def operate(self):
+        print("Bzzzz")
+        
+        
 class Microwave(Appliance):
-	def __init__(self,brand, power):
-		self.__brand = brand
-		self.__power = power
-	def heat(self):
-		print("Microvalnovka razogrevaet")
-	#@overriding
-	def operate(self):
-		print("wwwwww")
+    def heat(self):
+        print("Microvolnovka")
+    
+    def operate(self):
+        print("WWWWWW")
 		
 ap = Appliance(5,10)
 
 print(ap.get_brand())
 print(ap.get_power())
 
-WM = WashingMachine(1,10)
-
+WM = WashingMachine(5,10)
 WM.wash()
 WM.operate()
 
-RG = Refrigerator(1,10)
+RG = Refrigerator(5,10)
 RG.cool()
 RG.operate()
 
-MC = Microwave(1,10)
+MC = Microwave(5,10)
 MC.heat()
 MC.operate()
