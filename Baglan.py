cb498905f03adb57ff7834d02883d3ff7abed6bf
usnv999 <baglan.usenoff@gmail.com>
2024-11-18 13:33:41 +0600

    1 nuska

---
+++ Baglan.py
@@ -0,0 +1,31 @@
+Class University
+    def__init__(self, price):
+        self.price = price
+        print("University price is equal to: ")
+            git.add
+
+Class ITFaculty
+    def__init__(self, computers):
+        self.computers = computers
+        print("There are that amount of PC's: ")
+
+    return f"Computers: {self}"    
+
+Class EnergyFaculty
+    def__init__(self, electricity):
+        self.electricity = electricity
+        print("This faculty has an electricity: ")
+
+    return f"electricity: {self}"
+    
+Class EcoFaculty
+    def__init__(self, ecology):
+        self.ecology = ecology
+        print("Keep the ecology safety,pls: ")
+
+    return f"ecology: {self}"
+
+University = Price()
+Computers = PC's()
+electricity = electricity()
+ecology = ecology()
