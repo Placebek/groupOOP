edc283886d018bba648c3ebbd85657a154888d49
2202zhan <zklzanov@gmail.com>
2024-11-18 12:18:18 +0500

    3 nusqa

---
+++ exam2(1).py
@@ -0,0 +1,24 @@
+class Team:
+    def __init__(self, players, teams)
+        self.teams = []
+
+
+class Player:
+    def __init__(self, age, name)
+        self.age = []
+
+def AgePlayers(Player, Team):
+    get.player
+
+
+
+
+
+
+class Singleton:
+    _instance = None
+    def __new__(cls): 
+    return cls._instance or super().__new__(cls)
+
+s1, s2 = Singleton(), Singleton()
+print(s1 is s2)    
